cd53acfe93c91f5d650345689f654f57bc5d89bd
Ayushi Gorai <aygo@odoo.com>
2023-01-27 18:54:10 +0530

    self module with service model

--- fleetio/models/vehicle_service.py
+++ fleetio/models/vehicle_service.py
@@ -5,5 +5,13 @@ class Vehicle_Service(models.Model):
     _name = "vehicle.service"
     _description = "Fleet Service Model"
 
-    name=fields.Char()
+    service_type =fields.Char(string="Service Type")
+    description = fields.Char(string="Description")
+    date = fields.Date(string="Date")
+    cost = fields.Integer(string="Cost")
+    vendor = fields.Char(string="Vendor")
+    vehicle = fields.Char(string="Vehicle")
+    driver = fields.Char(string="Driver")
+    odometer_value = fields.Integer(string="Odometer")
+    notes = fields.Text()
 
